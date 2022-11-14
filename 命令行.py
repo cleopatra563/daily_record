09bde76f331ad17c9404686c6f54def345a8fdd2
cleopatra563 <cleopatrazhou563@gmailcom>
2022-11-14 09:15:49 +0800

    ff

---
+++ 命令行.py
@@ -0,0 +1,11 @@
+如何创建 Anaconda 虚拟环境
+
+# 版本切换
+打开Anaconda Prompt，比如创建一个名为Glenn 的python版本为3.7.3的环境
+# 创建虚拟环境 conda create -n Glenn python=3.7.3
+# 激活虚拟环境 conda activate Glenn
+# 退出虚拟环境 deactivate
+
+# 安装第三方库和版本号
+切换到python环境
+# 库与版本 pip list
