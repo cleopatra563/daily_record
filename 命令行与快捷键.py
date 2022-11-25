993a869d6cd509a0661b54258ce7e517011dd749
cleopatra563 <cleopatrazhou563@gmailcom>
2022-11-25 10:10:54 +0800

    lk

--- 命令行与快捷键.py
+++ 命令行与快捷键.py
@@ -4,8 +4,8 @@
 
 # 版本切换
 打开Anaconda Prompt，比如创建一个名为Glenn 的python版本为3.7.3的环境
-# 创建虚拟环境 conda create -n Glenn python=3.7.3
-# 激活虚拟环境 conda activate Glenn
+# 创建虚拟环境 conda create -n pyhton3.7 python=3.7.3
+# 激活虚拟环境 conda activate python3.7
 # 退出虚拟环境 deactivate
 
 # 安装第三方库和版本号
