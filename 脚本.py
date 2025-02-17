bdf60ac6190a794b32745c62815242ade89a68ff
cleopatra563 <cleopatrazhou563@gmailcom>
2025-02-17 08:56:09 +0800

    dd018

--- 脚本.py
+++ 脚本.py
@@ -1,7 +1,37 @@
-def circle(t,r,length):
+def circle(t,r,angle):
     C = 2*math.pi*r
-    n = C/length
+    length = C*abs(angle)/360
+    n = int(C/length)
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+n*angle = 360
+length*n = C
+C = 2*math.pi*r
+
+n = int(arc_length / 4) + 3
+t.fd(step_length)
+t.lt(step_angle)
+
+def polygon(t,n,length):
     angle = 360/n
     for i in range(n):
         t.fd(length)
         t.lt(angle)
+
+def circle(t,r):
+    C = 2*math.pi*r
+    n = 50
+    length = C/n
+    polygon(t,n,length)
+
+#设计函数接口，显示直接信息
+def circle(t,r):
+    C = 2*math.pi*r
+    n = int(C/3)+1
+    length = C/n
+    polygon(t,n,length)
+
+def circle(t,r):
+    arc(t,r,360)
